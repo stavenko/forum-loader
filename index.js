5c01ab2b9faffadb177f98e88ee61edaeb034153
Vasiliy G. Stavenko <stavenko@gmail.com>
2018-02-02 18:21:27 +0300

    create delayed retry on page downloading

--- index.js
+++ index.js
@@ -13,12 +13,53 @@ const rootUrl = 'https://bitcointalk.org/';
 const BOARDS_ON_PAGE = 40;
 const TOPICS_ON_PAGE = 20;
 const DEBUG_DOWNLOAD_BOARD_PAGES = 2;
+const pageDownloadPauseMin = 100;
+const pageDownloadPauseMax = 1000;
+const maximumRetries = 1000;
+
+let fetchRetries = 0;
+
+
+async function fetchPageWithText(url) {
+  return fetch(url).then(async res => {
+    if (res.ok && fetchRetries < maximumRetries) {
+      const page = await res.text();
+      const dom = new jsdom(page);
+      if (!dom.window.document) {
+        console.log(' no document');
+      }
+      fetchRetries = 0;
+
+      return {page, document: (new jsdom(page)).window.document};
+    } else {
+      ++fetchRetries;
+      const d = getDelauAmount();
+      console.log(`Retry #${fetchRetries}: Not ok respone, delay next download: ${d}`);
+      return delay(d).then(() => {
+        return fetchPageWithText(url);
+      });
+    }
+  });
+}
+
+function getDelauAmount() {
+  const d = pageDownloadPauseMax - pageDownloadPauseMin;
+  return pageDownloadPauseMin + Math.floor(Math.random() * d);
+}
+
+async function delay(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve();
+    }, time);
+  });
+}
+
+
 
 async function fetchPage(url) {
-  return fetch(url).then(async res => {
-    const page = await res.text();
-    return (new jsdom(page)).window.document;
-  });
+  const result = await fetchPageWithText(url);
+  return result.document;
 }
 
 async function getAllBoards(forumRoot) {
@@ -27,7 +68,7 @@ async function getAllBoards(forumRoot) {
   const boards = [];
   for (let c = 0; c < forumGroups.length - 1; ++c) {
     const bbs = retrieveBoardsFromGroups(forumGroups[c])
-    console.log('--->', bbs.length);
+    // console.log('--->', bbs.length);
     boards.push(...bbs);
   }
   return boards;
@@ -64,8 +105,8 @@ async function getAllTopics({boardName, boardUrl}) {
   const topics = getAllTopicsFromBoardPage(firstPageContent);
   for (let i = 1; i < totalPages; ++i) {
     const nextPageUrl = createNextBoardPageUrl(boardUrl, i);
-    const nextPageContent = await fetchPage(nextPageUrl);
-    const moreTopics = getAllTopicsFromBoardPage(nextPageContent);
+    const nextPageContent = await fetchPageWithText(nextPageUrl);
+    const moreTopics = getAllTopicsFromBoardPage(nextPageContent.document);
     topics.push(...moreTopics);
     if (DEBUG_DOWNLOAD_BOARD_PAGES && i >= DEBUG_DOWNLOAD_BOARD_PAGES) {
       break;
@@ -77,6 +118,10 @@ async function getAllTopics({boardName, boardUrl}) {
 function getAllTopicsFromBoardPage(boardContent) {
   const tables = [...boardContent.querySelectorAll('div.tborder>table.bordercolor')];
   const table = tables[tables.length - 1];
+  if (!table) {
+    console.log(`can't get board content`);
+    return [];
+  }
   const trs = [...table.querySelectorAll('tr')];
   return trs.splice(1, trs.length-1)
     .map(tr => tr.querySelectorAll('td')[2].querySelectorAll('a')[0])
@@ -116,24 +161,40 @@ function retrieveTotalBoardPages(boardContent) {
 }
 
 async function retrieveAllMessages({topic, topicRootUrl}, contentSavier, topicSavier){
-  const firstPageContent = await fetchPage(topicRootUrl);
-  const totalPages = retrieveTotalTopicPages(firstPageContent);
-  const messages = getAllMessagesFromTopicPage(firstPageContent);
-  console.log(`\tProcessing Page 1/${totalPages}`);
+  const firstPageContent = await fetchPageWithText(topicRootUrl);
+  const totalPages = retrieveTotalTopicPages(firstPageContent.document);
+  const messages = getAllMessagesFromTopicPageSafe(topicRootUrl, firstPageContent);
+  console.log(`\t\t* Processing Page 1/${totalPages}`);
   topicSavier(topic);
   contentSavier(messages);
   for (let i = 1; i < totalPages; ++i) {
-    console.log(`\tProcessing Page ${i+1}/${totalPages}`);
+    console.log(`\t\t* Processing Page ${i+1}/${totalPages}`);
     const nextPageUrl = createNextTopicPageUrl(topicRootUrl, i);
-    const nextPageContent = await fetchPage(nextPageUrl);
-    contentSavier(getAllMessagesFromTopicPage(nextPageContent));
+    const nextPageContent = await fetchPageWithText(nextPageUrl);
+    contentSavier(getAllMessagesFromTopicPageSafe(nextPageUrl, nextPageContent));
   }
   return messages;
+}
+
+function getAllMessagesFromTopicPageSafe(url, fetchResult) {
+  try {
+    return getAllMessagesFromTopicPage(fetchResult.document);
+  } catch (e) {
+    console.log('---- unexpected page content on forum url' + url);
+    console.log('=============== Page content:====================');
+    console.log(fetchResult.page);
+    console.log('=================================================');
+    return [];
+  }
 
 }
 
 function getAllMessagesFromTopicPage(topicPage) {
-  const tables = [...topicPage.querySelectorAll('form>table')[0]
+  const rawTables = topicPage.querySelectorAll('form>table');
+  if (rawTables.length === 0) {
+    throw new Error('Unexpected document content.');
+  } 
+  const tables = [...rawTables[0]
     .querySelectorAll('tr[class] td.windowbg>table, tr[class] td.windowbg2>table')];
   const messages = tables.map(t => t.querySelectorAll('tr')[0]
                                    .querySelectorAll('td')[1]
@@ -206,7 +267,10 @@ async function exists(file) {
 function parseOptions() {
   const switches = [
       ['-h', '--help', 'Shows help sections'],
-      ['-b', '--beginFromBoard NUMBER', 'Start parsing and saving from board number'],
+      ['-B', '--beginFromBoard NUMBER', 'Start parsing and saving from board number'],
+      ['-T', '--beginFromTopic NUMBER', 'Start parsing and saving from topic number'],
+      ['-b', '--print-boards', 'print boards and theys` numbers'],
+      ['-t', '--print-topics', 'print boards and theys` numbers'],
       ['-o', '--output PATH', 'File to save']
   ];
 
@@ -220,7 +284,12 @@ function parseOptions() {
   });
   parser.on('*', (f,v) => {
     options[f] = v;
-
+    if (f === 'print-topics') {
+      options[f] = true;
+    }
+    if (f === 'print-boards') {
+      options[f] = true;
+    }
   })
 
 
@@ -263,30 +332,54 @@ async function main() {
 }
 
 async function recursiveBoards(boards, options) {
+  const startWith = options.beginFromBoard - 1 || 0;
   const retriveBoard = async i => {
     if (boards[i]) {
       const boardName = boards[i].boardName;
       console.log(`Processing board #${i+1}/${boards.length}: ${boardName}`);
       const topics = await getAllTopics(boards[i]);
-      await recursiveTopics(topics, boardName, options);
+      let fromTopic = 0;
+      console.log(i, startWith);
+      if (i === startWith) {
+        fromTopic = options.beginFromTopic -1 || 0;
+      }
+      await recursiveTopics(topics, boardName, options, fromTopic);
       await retriveBoard(i + 1);
     }
   }
-  const startWith = options.beginFromBoard-1 || 0;
+  console.log(options);
+  if (options['print-boards']) {
+    printBoards(boards);
+  }
+
   return await retriveBoard(Math.max(0, startWith));
 }
+function printBoards(boards) {
+  for (let i = 0; i < boards.length; ++i) {
+    const {boardName} = boards[i];
+    console.log(i, boardName);
+  }
+}
 
-async function recursiveTopics(topics, boardName, options) {
+async function recursiveTopics(topics, boardName, options, fromTopic = 0) {
   const retriveTopics = async i => {
     if(topics[i]) {
       console.log(`\tProcessing topic #${i+1}/${topics.length}: ${topics[i].topic}`);
       const messageFilename = fileName(options, 'messages', boardName, 'txt');
       const topicFilename = fileName(options, 'topics', boardName, 'txt')
-      await retrieveAllMessages(topics[i], getMessagesSavier(messagesQueue, messageFilename), getTopicSavier(messagesQueue, topicFilename));
+      try {
+        await retrieveAllMessages(topics[i], getMessagesSavier(messagesQueue, messageFilename), getTopicSavier(messagesQueue, topicFilename));
+      } catch (e) {
+        console.log('\n\t\t\t******* WARNING ********\n\n');
+        console.log(`\t\t\tTopic "${topics[i].topic}" in board ${boardName} \n`);
+        console.log(`\t\t\tin board ${boardName} has error on downloading.\n`);
+        console.log(`Actual error was: ${e}`);
+        console.log('\n\t\t\t************************\n\n');
+      }
       await retriveTopics(i + 1);
     }
   }
-  return await retriveTopics(0);
+  return await retriveTopics(fromTopic);
 }
 
 function fileName(options, where, name, ext) {
